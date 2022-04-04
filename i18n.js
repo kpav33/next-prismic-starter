b07f699777b38a11235fc1b0a62214705a73ed58
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-04-04 11:22:58 +0200

    Change default locale to Slovene

--- i18n.js
+++ i18n.js
@@ -1,6 +1,6 @@
 module.exports = {
   locales: ["en", "sl"], // Array with the languages that you want to use
-  defaultLocale: "en", // Default language of your website
+  defaultLocale: "sl", // Default language of your website
   pages: {
     "*": ["common"], // Namespaces that you want to import per page (we stick to one namespace for all the application in this tutorial)
   },
